1ebf0d6592163d44625bfcc8ef14b91d215ca166
My name <user@example.com>
2021-02-25 13:21:49 -0600

    -

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog2`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Cris Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
